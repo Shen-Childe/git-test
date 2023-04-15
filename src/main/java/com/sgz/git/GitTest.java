d2adc29dbed78f47150212ab0f5eeb650f283122
Shen-Childe <87227653+Shen-Childe@users.noreply.github.com>
2023-04-15 17:41:55 +0800

    Update GitTest.java

--- src/main/java/com/sgz/git/GitTest.java
+++ src/main/java/com/sgz/git/GitTest.java
@@ -14,5 +14,8 @@ public class GitTest {
         System.out.println("push test!");
         System.out.println("push test!");
         System.out.println("pull test!");
+        System.out.println("push test!");
+        System.out.println("push test!");
+        System.out.println("pull test!");
     }
 }
