9a6bf3827cb544390578bd12b13712274fa4acb2
沈公子 <2298638744@qq.com>
2023-04-15 12:23:19 +0800

    1 commit

---
+++ src/main/java/com/sgz/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.sgz.git;
+
+/**
+ * @Auther：沈公子
+ * @Date：2023/4/15 - 10:24
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
